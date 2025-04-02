4c42933511fa6b4590d6edfa2f544cbd222de127
FrancoisleRoux <159991396+231256leRouxFNF@users.noreply.github.com>
2025-04-02 19:45:32 +0200

    NavBar
    Created a basic navbar component

--- afk-website/src/components/NavBar.js
+++ afk-website/src/components/NavBar.js
@@ -1,1 +1,24 @@
+import React from 'react';
+import './NavBar.css'; // Assuming you will style the NavBar in a CSS file
+const NavBar = () => {
+    return (
+        <nav className="navbar">
+            <div className="navbar-logo">
+                <a href="/">Logo</a>
+            </div>
+            <div className="navbar-links">
+                <a href="/home">Home</a>
+                <a href="/about">About</a>
+                <a href="/services">Services</a>
+                <a href="/contact">Contact</a>
+            </div>
+            <div className="navbar-buttons">
+                <button className="btn-signin">Sign In</button>
+                <button className="btn-signup">Sign Up</button>
+            </div>
+        </nav>
+    );
+};
+
+export default NavBar;
 

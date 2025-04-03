e0e65ec66f3a60e06564497790b8a2d51cf75f20
FrancoisleRoux <159991396+231256leRouxFNF@users.noreply.github.com>
2025-04-03 10:10:18 +0200

    Card and Form
    added the Card and the form components

--- afk-website/src/components/Form.js
+++ afk-website/src/components/Form.js
@@ -1,1 +1,49 @@
+import React from "react";
+// import image from "./image.svg";
+// import resizingHandle from "./resizing-handle.svg";
+import "./style.css";
+// import vector from "./vector.svg";
 
+export const Frame = () => {
+    return (
+        <div className="frame">
+            <div className="textbox">
+                <div className="textfield">
+                    {/* <img className="vector" alt="Vector" src={image} /> */}
+
+                    <div className="text-wrapper">Full name</div>
+                </div>
+
+                <div className="div">Your name</div>
+            </div>
+
+            <div className="textbox-2">
+                <div className="div">Your email</div>
+
+                <div className="textfield">
+                    {/* <img className="vector" alt="Vector" src={vector} /> */}
+
+                    <div className="text-wrapper">yourmail@emaily.com</div>
+                </div>
+            </div>
+
+            <div className="textarea">
+                <div className="div">How can we help?</div>
+
+                <div className="textfield-2">
+                    <div className="text-wrapper-2">Enter your message here</div>
+
+                    {/* <img
+                        className="resizing-handle"
+                        alt="Resizing handle"
+                        src={resizingHandle}
+                    /> */}
+                </div>
+            </div>
+
+            <button className="button">
+                <div className="text-wrapper-3">Send my message</div>
+            </button>
+        </div>
+    );
+};

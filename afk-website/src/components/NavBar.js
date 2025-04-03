79d5ef97a6ad437671a562e6bcc2d152b310d354
OpenWindow231312 <231312@virtualwindow.co.za>
2025-04-03 10:32:11 +0200

    navbar update

--- afk-website/src/components/NavBar.js
+++ afk-website/src/components/NavBar.js
@@ -2,46 +2,28 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import {
-  HomeIcon,
-  BookOpenIcon,
-  UsersIcon,
-  EnvelopeIcon,
-} from "@heroicons/react/24/outline";
+import "./Navbar.css"; // Make sure this is connected
 
 const CustomNavbar = () => {
   return (
     <Navbar expand="lg" bg="white" className="shadow-sm py-3">
       <Container>
-        <Navbar.Brand
-          as={Link}
-          to="/"
-          style={{
-            color: "#ff4fcf",
-            fontFamily: "Quicksand",
-            fontWeight: "700",
-            fontSize: "28px",
-          }}
-        >
+        <Navbar.Brand as={Link} to="/" className="brand-text">
           CodeSprouts
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbar-nav" />
         <Navbar.Collapse id="navbar-nav">
-          <Nav className="ms-auto" style={{ gap: "20px" }}>
-            <Nav.Link as={Link} to="/" className="nav-link-custom">
-              <HomeIcon className="icon" />
+          <Nav className="ms-auto nav-links">
+            <Nav.Link as={Link} to="/" className="nav-link-text">
               Home
             </Nav.Link>
-            <Nav.Link as={Link} to="/about" className="nav-link-custom">
-              <UsersIcon className="icon" />
+            <Nav.Link as={Link} to="/about" className="nav-link-text">
               About
             </Nav.Link>
-            <Nav.Link as={Link} to="/curriculum" className="nav-link-custom">
-              <BookOpenIcon className="icon" />
+            <Nav.Link as={Link} to="/curriculum" className="nav-link-text">
               Curriculum
             </Nav.Link>
-            <Nav.Link as={Link} to="/contact" className="nav-link-custom">
-              <EnvelopeIcon className="icon" />
+            <Nav.Link as={Link} to="/contact" className="nav-link-text">
               Enroll
             </Nav.Link>
           </Nav>

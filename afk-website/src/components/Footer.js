6af888b673273e5dc527f1b851674e364b8a99dd
FrancoisleRoux <159991396+231256leRouxFNF@users.noreply.github.com>
2025-04-10 09:41:53 +0200

    footer
    Changed the bg-colour of the footer too white and the text black

--- afk-website/src/components/Footer.js
+++ afk-website/src/components/Footer.js
@@ -3,15 +3,14 @@ import './Footer.css'; // Optional: Add styles for the footer
 
 const Footer = () => {
   return (
-    <footer style={{ backgroundColor: '#333', color: '#fff', padding: '1rem', textAlign: 'center' }}>
+    <footer style={{ backgroundColor: '#FFFFF', color: '#000', padding: '1rem', textAlign: 'center' }}>
       <p>&copy; {new Date().getFullYear()} AFK Spot. All rights reserved.</p>
       <p>
-        <a href="/privacy-policy" style={{ color: '#fff', textDecoration: 'none' }}>Privacy Policy</a> | 
-        <a href="/terms-of-service" style={{ color: '#fff', textDecoration: 'none', marginLeft: '0.5rem' }}>Terms of Service</a>
+        <a href="/privacy-policy" style={{ color: '#000', textDecoration: 'none' }}>Privacy Policy</a> | 
+        <a href="/terms-of-service" style={{ color: '#000', textDecoration: 'none', marginLeft: '0.5rem' }}>Terms of Service</a>
       </p>
     </footer>
   );
 };
 
 export default Footer;
-
